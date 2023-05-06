d0e4e103b11e5bf0a7fbde52c223cbf0f570d014
bur1to <daniil.dedenok@gmail.com>
2023-05-06 22:08:07 +0300

    refactoring dto models and controllers

--- userEventsASP.NET/Dto/BlogDto.cs
+++ userEventsASP.NET/Dto/BlogDto.cs
@@ -2,6 +2,7 @@
 {
     public class BlogDto
     {
+        public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public string Title { get; set; }
         public string Themes { get; set; }
